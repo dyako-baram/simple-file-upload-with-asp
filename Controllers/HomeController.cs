415a600226bb07c31b3540765608dba72e2e6c2c
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-01-15 14:41:34 +0300

    change to IWebHostEnvironment

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using fu.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace fu.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment env;
+
+        public HomeController(ILogger<HomeController> logger,IWebHostEnvironment env)
+        {
+            _logger = logger;
+            this.env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            var dir=env.ContentRootPath+"/wwwroot/images/";
+            var name=Guid.NewGuid().ToString("N")+"_"+file.FileName;
+            using (var stream=new FileStream(Path.Combine(dir,name),FileMode.Create,FileAccess.Write))
+            {
+                file.CopyToAsync(stream);
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
